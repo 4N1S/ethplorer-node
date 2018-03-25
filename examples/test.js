01ed465a9f740820ebfded8e47c1fb7f65a770d9
NiS <NiS@MacBook-Air-de-Ludovic.local>
2018-03-26 01:42:55 +0200

    Release 0.0.3

--- examples/test.js
+++ examples/test.js
@@ -3,7 +3,14 @@ var ethplorerClient = require('../index.js');
 var api_key="freekey";
 var client = new ethplorerClient();
 var limit="5";
-client.getTokenHistory(limit,function (error, data) {
+// client.getTokenHistory(limit,function (error, data) {
+// 	if(error) console.log("E!",error)
+// 	console.dir(data);
+// });
+
+var criteria="cap";
+var limit="5";
+client.getTop(api_key,criteria,limit,function (error, data) {
 	if(error) console.log("E!",error)
 	console.dir(data);
 });
